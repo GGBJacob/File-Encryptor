d37eacc83a482ceb36bf6f6462281c6f5e16b011
Jakub Romanowski <58742151+GGBJacob@users.noreply.github.com>
2024-05-17 15:46:54 +0200

    added creating directory
    added a function creating a directory with result files
    
    added temporary file selection block to UI

--- Cyberprojekt/main.py
+++ Cyberprojekt/main.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
@@ -6,7 +7,7 @@ from encryption import KEY_LENGTH
 
 
 def get_file(entry, input_file, filetypes):
-    filepath = fd.askopenfilename(title='Wybierz plik', filetypes=filetypes)
+    filepath = fd.askopenfilename(title='Select file', filetypes=filetypes)
     entry.config(state='normal')
     if not filepath:
         input_file.set("Null")
@@ -26,7 +27,7 @@ def reset_form(arguments):
 
 
 def save_file(entry, file, filetypes):
-    filepath = fd.asksaveasfilename(title="Wybierz plik", defaultextension=filetypes[-1][1],
+    filepath = fd.asksaveasfilename(title="Select file", defaultextension=filetypes[-1][1],
                                     filetypes=filetypes)
     entry.config(state='normal')
     if not filepath:
@@ -53,6 +54,49 @@ def check_file_name(output_name, input_file):
     elif tmp[len(tmp) - 1] != "txt":
         mb.showerror("Error", "Invalid file extension!\nAccepted extensions: *.txt")
         return
+
+
+def create_encryption_directory(input_file, public_key, private_key, folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    input_file_name = input_file.get().split('/')[-1].split('.')[0]
+
+    file_names = ['encrypted_' + input_file_name + '.txt', 'public_key.pub', 'private_key']
+
+    file_paths = []
+
+    files = 0
+    for file_name in file_names:
+        file_path = os.path.join(folder_path, file_name)
+        file_paths.append(file_path)
+        with open(file_path, 'w') as file:
+            match files:
+                case 0:
+                    file.write('')
+                case 1:
+                    file.write(public_key)
+                case 2:
+                    file.write(private_key)
+        files += 1
+
+    output_file, public_key, private_key = file_paths
+    return output_file, public_key, private_key
+
+
+def get_directory(entry, input_dir):
+    directory = fd.askdirectory(title='Select file')
+    entry.config(state='normal')
+    if not directory:
+        input_dir.set("Null")
+    else:
+        input_dir.set(directory)
+    entry.config(state='readonly')
+
+    # TESTS
+    temp = tk.StringVar(value="X:/temp.txt")
+    create_encryption_directory(temp, "ABCD", "WOMP WOMP", directory)
+
 
 def create_encryption_UI(frame, input_file, key_value, key_file, output_file):
     # Napis 1
@@ -115,7 +159,7 @@ def create_encryption_UI(frame, input_file, key_value, key_file, output_file):
     label_encrypt_mode.grid(row=4, column=0, padx=5, pady=10)
 
     # Wybór szyfrowania
-    encrypt_mode = tk.StringVar(value="block") #Tryb szyfrowania
+    encrypt_mode = tk.StringVar(value="block")  # Tryb szyfrowania
     rbutton1 = tk.Radiobutton(frame, text="Block", value="block", variable=encrypt_mode)
     rbutton2 = tk.Radiobutton(frame, text="Stream", value="stream", variable=encrypt_mode)
 
@@ -127,11 +171,30 @@ def create_encryption_UI(frame, input_file, key_value, key_file, output_file):
                         command=lambda: enc.encrypt_sym(input_file, output_file, key_file, key_value, encrypt_mode))
     confirm.grid(row=5, column=0, padx=10, pady=10)
 
-
     # Przycisk clear
     button_clear = tk.Button(frame, text="Clear",
                              command=lambda: reset_form([input_file, key_file, key_value, output_file]))
     button_clear.grid(row=5, column=2, pady=10, padx=10)
+
+    # ~~~~~~~~~ TEMP tworzenie plików ~~~~~~~~~
+
+    # Label
+
+    path_label = tk.Label(frame, text="Result path")
+    path_label.grid(row=6, column=0, padx=5, pady=10)
+
+    # Path
+
+    path = tk.StringVar(value="")
+    path_entry = tk.Entry(frame, textvariable=path)
+    path_entry.config(state='readonly')
+    path_entry.grid(row=6, column=1, padx=5, pady=10)
+
+    # Button
+    path_select_button = tk.Button(frame, text="Select path", command=lambda: get_directory(path_entry, path))
+    path_select_button.grid(row=6, column=2, padx=5, pady=10)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 def create_decryption_UI(frame, input_file, key_file, output_file):
@@ -178,7 +241,8 @@ def create_decryption_UI(frame, input_file, key_file, output_file):
     button3.grid(row=2, column=2, padx=5, pady=10)
 
     # Przycisk decrypt
-    button_decrypt = tk.Button(frame, text="Decrypt!", command=lambda: enc.decrypt_file(input_file, key_file, output_file))
+    button_decrypt = tk.Button(frame, text="Decrypt!",
+                               command=lambda: enc.decrypt_file(input_file, key_file, output_file))
     button_decrypt.grid(row=3, column=0, padx=5, pady=10)
 
     # Przycisk clear
@@ -217,7 +281,7 @@ def main():
     decrypt_frame = tk.Frame()
 
     create_decryption_UI(decrypt_frame, input_file, key_file, output_file)
-    create_encryption_UI(encrypt_frame, input_file, key_value, key_file,output_file)
+    create_encryption_UI(encrypt_frame, input_file, key_value, key_file, output_file)
 
     if appMode.get() == 1:
         change_frame(decrypt_frame, encrypt_frame)
